a0b21592c388c3a76564d27a23c77a367a9b2cf4
유혁 <529703@naver.com>
2021-11-30 01:37:17 +0900

    led.h
    하나씩 다시 올려봄

--- led.h
+++ led.h
@@ -1,7 +1,7 @@
 #ifndef _LED_LIBRARY_H_
 #define _LED_LIBRARY_H_
 
-int ledLibInit(void);	//이 라이브러리는 LED를 켜기 전에 반드시 한 번 불러주어야 함
+int ledLibInit(void); //이 라이브러리는 LED를 켜기 전에 반드시 한 번 불러주어야 함
 int ledLibOnOff(int ledNumber, int onOff); //이 함수는 ledNumber에는 0~7d을 넣으면, 그에 해당하는 led를 켜거나 끄거나 해준다.
 int ledLibStatus(void);
 int ledLibExt(void);
