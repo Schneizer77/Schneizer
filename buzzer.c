a6e241bf863494b71a8b0e08a89a5c1d62e5c4b7
Woojin Choi <cwj0717@naver.com>
2021-12-04 18:40:05 +0900

    buzzer.h
    buzzer.h

---
+++ buzzer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#include "buzzer.h"
+
+int buzzerInit(void)
+{
+
+}
+
+int buzzerPlaySong(int scale)
+{
+
+}
+
+int buzzerStopSong(void)
+{
+
+}
+
+int buzzerExit(void)
+{
+
+}
