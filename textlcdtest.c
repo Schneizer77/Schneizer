d6fa88b23bf5651dcd632c780299db14fb45f4bd
유혁 <529703@naver.com>
2021-12-14 04:29:37 +0900

    textlcdtest.c 수정
    수정

--- textlcdtest.c
+++ textlcdtest.c
@@ -14,70 +14,22 @@
 
 #define TEXTLCD_DRIVER_NAME		"/dev/peritextlcd"
 
-void doHelp(void)
-{
-	printf("usage: textlcdtest <linenum> <'string'>\n");
-	printf("       linenum => 1 ~ 2\n");	
-	printf("  ex) textlcdtest 2 'test hello'\n");
-	
-}
 
 
-int main(int argc , char **argv)
-{
-	unsigned int linenum = 0;
+int main(void)
+{   
+    int one = 1;
+    int two = 2;
+    char abc[10] = "Hello";
+    char xyz[10] = "embeded";
 
-	stTextLCD  stlcd; 
-	int fd;
-	int len; 
-	
-	memset(&stlcd,0,sizeof(stTextLCD));
-	
-	if (argc < 3 )
-	{
-		perror(" Args number is less than 2\n");
-		doHelp();
-		return 1;
-	}
-	
-	linenum = strtol(argv[1],NULL,10);
-	printf("linenum :%d\n", linenum);
-	
-	if ( linenum == 1) // firsst line
-	{
-		stlcd.cmdData = CMD_DATA_WRITE_LINE_1;
-	}
-	else if ( linenum == 2) // second line
-	{
-		stlcd.cmdData = CMD_DATA_WRITE_LINE_2;
-	}
-	else 
-	{
-		printf("linenum : %d  wrong .  range (1 ~ 2)\n", linenum);
-		return 1; 
-	}
-	printf("string:%s\n",argv[2]);
-	len = strlen(argv[2]);
-	if ( len > COLUMN_NUM)
-	{
-		memcpy(stlcd.TextData[stlcd.cmdData - 1],argv[2],COLUMN_NUM);
-	}
-	else
-	{
-		memcpy(stlcd.TextData[stlcd.cmdData - 1],argv[2],len);
-	}
-	stlcd.cmd = CMD_WRITE_STRING;
-	// open  driver 
-	fd = open(TEXTLCD_DRIVER_NAME,O_RDWR);
-	if ( fd < 0 )
-	{
-		perror("driver (//dev//peritextlcd) open error.\n");
-		return 1;
-	}
-	write(fd,&stlcd,sizeof(stTextLCD));
+    lcdtextInit();
 
-	
-	close(fd);
-	
-	return 0;
+    lcdtextWrite(one, abc);
+    lcdtextWrite(two, xyz);
+    sleep(5);
+    
+    lcdtextOff();
+
+    return 0;
 }
