a402e8ec81da54d180025abdef31727f56655271
유혁 <529703@naver.com>
2021-12-14 04:30:26 +0900

    colorledtest.c수정
    수정

--- colorledtest.c
+++ colorledtest.c
@@ -1,162 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <ctype.h>
-#include <sys/ipc.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+#define COLOR_LED_DEV_R_ "/sys/class/pwm/pwmchip0/"
+#define COLOR_LED_DEV_G_ "/sys/class/pwm/pwmchip1/"
+#define COLOR_LED_DEV_B_ "/sys/class/pwm/pwmchip2/"
+#define PWM_EXPORT "export"
+#define PWM_UNEXPORT "unexport"
+#define PWM_DUTY "pwm0/duty_cycle"
+#define PWM_PERIOD "pwm0/period"
+#define PWM_ENABLE "pwm0/enable"
+#define PWM_COLOR_R 0
+#define PWM_COLOR_G 1
+#define PWM_COLOR_B 2
+#define PWM_PERIOD_NS 1000000 //ns. = 1ms = 1khz 
+
 #include "colorled.h"
 
-#define MAX_SCALE_STEP		7
+int main(void) 
+{   
+    //---입력한 온도 값이라고 생각---//
+    double a = 100;
+    double b = 70;
+    double c = 50;
+    double d = 30;
+    
+    //---led 초기셋팅---//
+    pwmLedInit();
 
-#define PWM_BASE_SYS_PATH       "/sys/class/pwm/pwmchip"
+    //---개별 조명 컨트롤---//
+    ColorLED_White();
+    sleep(1);
+    ColorLED_Red();
+    sleep(1);
+    ColorLED_Green();
+    sleep(1);
+    ColorLED_Blue();
+    sleep(1);
+    
+    //---온도값에 따른 색상 변화 함수---//
+    pwmLed_thermalcheckmode(a);
+    sleep(2);
+    pwmLed_thermalcheckmode(b);
+    sleep(2);
+    pwmLed_thermalcheckmode(c);
+    sleep(2);
+    pwmLed_thermalcheckmode(d);
+    sleep(2);
+    
+    //---led끄기 및 종료---//
+    pwmLedOff();
 
-#define PWM_RED_BASE_SYS_PATH	"/sys/class/pwm/pwmchip0/pwm0/"
-#define PWM_GREEN_BASE_SYS_PATH	"/sys/class/pwm/pwmchip1/pwm0/"
-#define PWM_BLUE_BASE_SYS_PATH	"/sys/class/pwm/pwmchip2/pwm0/"
-
-#define PWM_SHOW	"1"
-#define PWM_HIDE	"0"
-
-#define PWM_ENABLE_NAME		"enable"
-#define PWM_FREQUENCY_NAME	"period"
-#define PWM_DUTYCYCLE_NAME	"duty_cycle"
-
-#define RED_INDEX	0
-#define GREEN_INDEX	1
-#define BLUE_INDEX	2
-
-#define TRUE	1
-#define FALSE	0
-
-#define  PWM_FREQUENCY		100000
-
-#define  MAX_INPUT_VALUE	100
-
-void doHelp(void)
-{
-	printf("Usage:Color LED\n");
-	printf("colorledtest  red green blue\n");
-	printf("color range 0 ~ 100\n");
-	printf("ex) colorledtest  100 100 100 =>white color, all full on\n");
-}
-
-void pwmActivate(int bActivate, int pwmIndex)
-{
-	char	strshellcmd[150];
-	if ( bActivate)
-	{
-		sprintf(strshellcmd,"echo '0' > %s%d/export\n",PWM_BASE_SYS_PATH,pwmIndex);
-		printf(strshellcmd);
-		system(strshellcmd);
-	}
-	else
-	{
-		sprintf(strshellcmd,"echo '0' > %s%d/unexport\n",PWM_BASE_SYS_PATH,pwmIndex);
-		printf(strshellcmd);
-		system(strshellcmd);
-	}
-}
-
-void pwmEnable(int bEnable , int pwmIndex)
-{
-	char	strshellcmd[150];	
-	if ( bEnable)
-	{
-		sprintf(strshellcmd,"echo '1' > %s%d/pwm0/%s\n",PWM_BASE_SYS_PATH,pwmIndex,PWM_ENABLE_NAME);
-		printf(strshellcmd);
-		system(strshellcmd);
-	}
-	else
-	{
-		sprintf(strshellcmd,"echo '0' > %s%d/pwm0/%s\n",PWM_BASE_SYS_PATH,pwmIndex,PWM_ENABLE_NAME);
-		printf(strshellcmd);
-		system(strshellcmd);
-	}
-}
-
-// 65537 ~ 1000000000  Hz
-void writePWMPeriod(int frequency, int pwmIndex) 
-{
-	char	strshellcmd[150];	
-	sprintf(strshellcmd,"echo '%d' > %s%d/pwm0/%s\n",frequency,PWM_BASE_SYS_PATH, pwmIndex,PWM_FREQUENCY_NAME);
-	printf(strshellcmd);
-	system(strshellcmd);	
-}
-
-void writePWMDuty(int  DutyCycle , int pwmIndex)
-{
-	char	strshellcmd[150];	
-	sprintf(strshellcmd,"echo '%d' > %s%d/pwm0/%s\n",DutyCycle,PWM_BASE_SYS_PATH,pwmIndex,PWM_DUTYCYCLE_NAME);
-	printf(strshellcmd);
-	system(strshellcmd);	
-}
-
-int main(int argc , char **argv)
-{
-	int red,green,blue;
-	int redduty,greenduty,blueduty;
-	
-	if (argc < 4 )
-	{
-		perror(" Args number is less than 4\n");
-		doHelp();
-		return 1;
-	}
-
-	red = atoi(argv[1]);
-	green = atoi(argv[2]);
-	blue = atoi(argv[3]);
-	
-	if ( (red > MAX_INPUT_VALUE) || (red < 0))
-	{
-		perror("red over range.\n");
-		doHelp();
-		return 1;
-	}
-
-	if ( (green > MAX_INPUT_VALUE) || (green < 0))
-	{
-		perror("green over range.\n");
-		doHelp();
-		return 1;
-	}
-
-	if ( (blue > MAX_INPUT_VALUE) || (blue < 0))
-	{
-		perror("blue over range.\n");
-		doHelp();
-		return 1;
-	}
-	
-	// inverse
-	red = MAX_INPUT_VALUE - red;
-	green = MAX_INPUT_VALUE - green;
-	blue = MAX_INPUT_VALUE - blue;
-	
-	// percentage
-	redduty = PWM_FREQUENCY * red / MAX_INPUT_VALUE;
-	greenduty = PWM_FREQUENCY * green / MAX_INPUT_VALUE;
-	blueduty = PWM_FREQUENCY * blue / MAX_INPUT_VALUE;
-	
-	pwmActivate(TRUE,RED_INDEX );
-	writePWMPeriod(PWM_FREQUENCY, RED_INDEX);
-	writePWMDuty(redduty, RED_INDEX);
-	pwmEnable(TRUE, RED_INDEX);
-	
-	pwmActivate(TRUE,GREEN_INDEX );
-	writePWMPeriod(PWM_FREQUENCY, GREEN_INDEX);
-	writePWMDuty(greenduty, GREEN_INDEX);
-	pwmEnable(TRUE, GREEN_INDEX);
-	
-	pwmActivate(TRUE,BLUE_INDEX );
-	writePWMPeriod(PWM_FREQUENCY, BLUE_INDEX);
-	writePWMDuty(blueduty, BLUE_INDEX);
-	pwmEnable(TRUE, BLUE_INDEX);
-	
-	return 0;
-	
+    return 0;
 }
